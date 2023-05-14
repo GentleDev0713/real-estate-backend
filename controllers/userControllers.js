c80f5682ea846da3df21c3eb99d31a689d27efc6
GentleDev0713 <vladimirleovac7@gmail.com>
2023-05-14 19:19:43 +0100

    commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callBack) => {
     const fileName = Date.now() + file.originalname;
-    req.body.picture.push("uploads/profiles/" + fileName);
+    req.body.pic.push("uploads/profiles/" + fileName);
 
     callBack(null, fileName);
   },
@@ -108,80 +108,3 @@ const allUsers = asyncHandler(async (req, res) => {
   res.send(users);
 });
 module.exports = { registerUser, authUser, allUsers };
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const asyncHandler = require("express-async-handler");
-// const generateToken = require("../config/generateToken");
-// const User = require("../Modelss/userModel");
-
-// const registerUser = asyncHandler(async (req, res) => {
-//   const { name, email, password, pic } = req.body;
-
-//   if (!name || !email || !password) {
-//     res.status(400);
-//     throw new Error("Please Enter All The Fields");
-//   }
-
-//   const userExists = await User.findOne({ email });
-
-//   if (userExists) {
-//     res.status(400);
-//     throw new Error("User Alredy Exists");
-//   }
-
-//   const user = await User.create({
-//     name,
-//     email,
-//     password,
-//     pic,
-//   });
-//   if (user) {
-//     res.status(201).json({
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       pic: user.pic,
-//       token: generateToken(user._id),
-//     });
-//   } else {
-//     res.status(400);
-//     throw new Error("Failed to Create User");
-//   }
-// });
-
-// const authUser = asyncHandler(async (req, res) => {
-//   const { email, password } = req.body;
-
-//   const user = await User.findOne({ email });
-
-//   if (user && (await user.matchPassword(password))) {
-//     res.json({
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       pic: user.pic,
-//       token: generateToken(user._id),
-//     });
-//   } else {
-//     res.status(400);
-//     throw new Error("Failed to Login");
-//   }
-// });
-
-// const allUsers = asyncHandler(async (req, res) => {
-//   const keyword = req.query.search
-//     ? {
-//         $or: [
-//           { name: { $regex: req.query.search, $options: "i" } },
-//           { email: { $regex: req.query.search, $options: "i" } },
-//         ],
-//       }
-//     : {};
-
-//   const users = await await User.find(keyword).find({
-//     _id: { $ne: req.user._id },
-//   });
-//   res.send(users);
-// });
-// module.exports = { registerUser, authUser, allUsers };
